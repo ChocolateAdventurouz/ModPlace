146f3be1e309dcda74cacf9beeb1e9dd84c65e0e
Karapatakis Aggelos <92590193+ChocolateAdventurouz@users.noreply.github.com>
2024-04-03 23:17:16 +0300

    Modplace features backend bringup

--- scripts/modplace.m
+++ scripts/modplace.m
@@ -1,7 +1,5 @@
 /*
-Only good llamas can access that :P
-
-Patched files:
+Modified files:
 
 - infoline.m
 - suicore.m
@@ -15,14 +13,12 @@ Patched files:
 - ../xml/modplace.xml
 - ../xml/modplace-normal.xml
 - ../skin.xml
-
-
 */
 
-#include "lib/std.mi"
-#include "lib/debug.m"
+#include <lib/std.mi>
+#include <lib/debug.m>
 #include "attribs/init_modplace.m"
-Global Button brw_enable_disable, coverplaying_enable_disable, file_labels_enable_disable, antialias_mcv, permanent_branding_enable_disable, video_to_art;
+Global Button brw_enable_disable, coverplaying_enable_disable, file_labels_enable_disable, antialias_mcv, permanent_branding_enable_disable, video_to_art, search_enable_disable, use_oldtimer;
 Global Group modplace_g;
 
 System.onScriptLoaded(){
@@ -34,7 +30,10 @@ System.onScriptLoaded(){
     antialias_mcv = modplace_g.findObject("antialias_mcv");
     permanent_branding_enable_disable = modplace_g.findObject("permanent_branding_enable_disable");
     video_to_art = modplace_g.findObject("video_to_art");
+    search_enable_disable = modplace_g.findObject("search_enable_disable");
+    use_oldtimer = modplace_g.findObject("use_oldtimer");
 }
+
 
 brw_enable_disable.onLeftClick(){
     
@@ -81,12 +80,28 @@ antialias_mcv.onLeftClick(){
         attr_antialias_mcv.setData("1");
     }
 }
+
+search_enable_disable.onLeftClick(){
+    if (attr_search_enable_disable.getData() == "1"){
+        attr_search_enable_disable.setData("0");
+    }
+    else {
+        attr_search_enable_disable.setData("1");
+    }
+}
 video_to_art.onLeftClick(){
     if (attr_art_to_video.getData() == "1"){
         attr_art_to_video.setData("0");
     }
     else {
-        messagebox("That mod is unstable, and it will probably lead to crashes, and errors. The skin will refresh after this message.", "Warning", 0, "");
         attr_art_to_video.setData("1");
+    }
+}
+use_oldtimer.onLeftClick(){
+    if (attr_oldtimer.getData() == "1"){
+        attr_oldtimer.setData("0");
+    }
+    else {
+        attr_oldtimer.setData("1");
     }
 }
